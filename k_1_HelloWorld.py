453c53815ea2e7b0d154d8259306d79f942e75d5
Royalkarang <122381186+Royalkarang@users.noreply.github.com>
2023-01-18 21:58:53 +0530

    Add files via upload

---
+++ k_1_HelloWorld.py
@@ -0,0 +1,6 @@
+# print("Hello World")
+
+"Taking input and pass output with Greeting "
+
+name = input("Enter Your name : ")
+print("Hello Dear, ",name )
---
+++ k_2_Airthmatic.py
@@ -0,0 +1,67 @@
+###############   Addition   ###################
+
+
+            #integer Input and Addition
+number_1= int(input("Enter Your First Number : "))
+number_2 = int(input("Enter Your Second Number : "))
+#we can directly add 
+#print(number_1+number_2)
+Addition = number_1+number_2
+print('Addition of two Number is ',Addition)
+            #FLoat Input and Addition
+"\t \t Float Addition "
+num1 = float(input("Enter 1st Number: "))
+num2 = float(input("Enter 2nd Number : "))
+print("Addition of FLoat number is : ",num1 + num2)
+
+
+###############   Subtraction   ###################
+
+
+            #integer Input and Subtraction
+number_1= int(input("Enter Your First Number : "))
+number_2 = int(input("Enter Your Second Number : "))
+#we can directly add 
+#print(number_1+number_2)
+sub = number_1 - number_2
+print('Subtraction of two Number is ',sub)
+            #FLoat Input and Subtraction
+"\t \t Float Addition "
+num1 = float(input("Enter 1st Number: "))
+num2 = float(input("Enter 2nd Number : "))
+print("Subtraction of FLoat number is : ",num1 - num2)
+
+
+###############   Multiplication   ###################
+
+
+            #integer Input and Multiplication
+number_1= int(input("Enter Your First Number : "))
+number_2 = int(input("Enter Your Second Number : "))
+#we can directly add 
+#print(number_1+number_2)
+mult = number_1 * number_2
+print('Multiplication of two Number is ',mult)
+            #FLoat Input and Addition
+"\t \t Float Addition "
+num1 = float(input("Enter 1st Number: "))
+num2 = float(input("Enter 2nd Number : "))
+print("Multiplication of FLoat number is : ",num1 * num2)
+
+
+
+###############   Divide   ###################
+
+
+            #integer Input and Multiplication
+number_1= int(input("Enter Your First Number : "))
+number_2 = int(input("Enter Your Second Number : "))
+#we can directly add 
+#print(number_1+number_2)
+mult = number_1 / number_2
+print('Multiplication of two Number is ',mult)
+            #FLoat Input and Multiplication
+"\t \t Float Addition "
+num1 = float(input("Enter 1st Number: "))
+num2 = float(input("Enter 2nd Number : "))
+print("Multiplication of FLoat number is : ",num1 / num2)
